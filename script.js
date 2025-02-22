02e91cf1985113be062c9a6b11eecdfbc79fd98a
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-22 16:01:27 -0500

    Added Files

---
+++ script.js
@@ -0,0 +1,11 @@
+const col = document.querySelector('.col');
+for(let j=0; j<16; j++) {
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    for(let i = 0; i<16; i++) {
+        let squareBox = document.createElement('div');
+        squareBox.setAttribute('class', 'square');
+        row.append(squareBox);
+    }
+    col.append(row);
+}
