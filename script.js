2b191da04afa2f6909d0e420bd99f47307a6d180
Zahir <176171770+Zahir091@users.noreply.github.com.>
2025-02-24 00:49:59 -0500

    Added Double Click Function for Drawing

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const col = document.querySelector('.col');
+let drawMode = false;
+
 for(let j=0; j<16; j++) {
     let row = document.createElement('div');
     row.setAttribute('class', 'row');
@@ -10,8 +12,19 @@ for(let j=0; j<16; j++) {
     col.append(row);
 }
 
+
+col.addEventListener('dblclick', function() {
+    drawMode = !drawMode; //toggling between states based on double click function
+    if (drawMode) {
+        document.body.style.cursor = "url('pencil-cursor.png'), auto"; 
+    } else {
+        document.body.style.cursor = "default"; 
+    }
+});
 document.querySelectorAll('.square').forEach(square => {
     square.addEventListener('mouseenter', function(){
-        square.style.backgroundColor = 'pink';
+        if(drawMode) {
+            square.style.backgroundColor = 'pink';
+        }
     });
 });
